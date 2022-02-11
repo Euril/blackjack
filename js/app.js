7e0102223423b9d0f956520d06ca6a4b55f07297
EugeneR <eugenerillo@gmail.com>
2022-02-11 13:40:00 -0500

    cardValue progress

--- js/app.js
+++ js/app.js
@@ -20,9 +20,9 @@ let discardDeck
 
 let blackjackDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
-//definded elsewhere in app.js
-// let playerTotal
-// let dealerTotal
+let playerTotal
+let dealerTotal
+let totalHolder
 
 /*------------------------ Cached Element References ------------------------*/
 let playerMenu = document.getElementById('btn-group')
@@ -31,13 +31,13 @@ let playGame = document.getElementById('messageArea')
 let playerHandElem = document.getElementById('playerHand')
 let dealerHandElem = document.getElementById('dealerHand')
 
-let player1Total = document.getElementById('playerTotal')
-let dealerTotal = document.getElementById('dealerTotal')
+player1Total = document.getElementById('playerTotal')
+dealerTotal = document.getElementById('dealerTotal')
 
 /*----------------------------- Event Listeners -----------------------------*/
 document.querySelector('.btn-group').addEventListener('click', evt =>{
   if(evt.target.id === 'hit'){
-    console.log("did the hit")
+    //console.log("did the hit")
     handleHit()
   }
   if(evt.target.id === 'stand'){
@@ -64,8 +64,8 @@ function drawCard(){
 //function for first turn draw phase
 function drawHands(){
   handleHit()
-  setTimeout(dealerHit, 2000)
-  setTimeout(handleHit, 3000)
+  setTimeout(dealerHit, 1000)
+  setTimeout(handleHit, 2500)
   setTimeout(dealerHit, 4000)
 }
 
@@ -81,6 +81,27 @@ function dealerHit(){
   dRender(cardPicked)
 }
 
+//function to add card value
+function cardValue(card){
+  let valueHolder
+  valueHolder = card.slice(1)
+  
+  if(isNaN(parseInt(valueHolder)) === true){
+    if(valueHolder === 'Q' || valueHolder === 'K' || valueHolder === 'J'){
+      console.log(10)
+    }
+    if(valueHolder ==='A'){
+      console.log('more Logic needed ere')
+    }
+  }
+  else{
+    console.log(parseInt(valueHolder))
+  }
+  // console.log(parseInt(valueHolder))
+  
+  }
+
+
 function p1Render(cardPicked){
   // Remove outline class when first card is picked
   if (playerHand.length === 1) {  
@@ -90,9 +111,10 @@ function p1Render(cardPicked){
     if(playerHand.length > 1){
       playerHandElem.classList.remove(p1CardToRemove)
     }
-  console.log(cardPicked)
+  //console.log(cardPicked)
   p1CardToRemove=cardPicked
   playerHandElem.classList.add(cardPicked)
+  cardValue(cardPicked)
   console.log(`Player: ${playerHand}`)
 }
 
@@ -105,8 +127,9 @@ function dRender(cardPicked){
     if(dealerHand.length > 1){
       dealerHandElem.classList.remove(dCardToRemove)
     }
-  console.log(cardPicked)
+  //console.log(cardPicked)
   dCardToRemove=cardPicked
   dealerHandElem.classList.add(cardPicked)
+  cardValue(cardPicked)
   console.log(`Dealer: ${dealerHand}`)
 }
